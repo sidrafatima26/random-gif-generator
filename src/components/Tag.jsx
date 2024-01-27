36fd4f9e937a962d2dd9cb13b4b123822116791a
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2024-01-27 19:37:47 +0530

    Update Tag.jsx

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -10,26 +10,7 @@ export default function Tag() {
   // const [gif, setGif] = useState('');
   // const [loading, setLoading] = useState(false);
   const [tag, setTag] = useState('');
-  //const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-
-
-//This function fetches gifs from the API and sets it to the state variable
-  // async function fetchData(){
-  //   setLoading(true);
-  //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
-  //   //const output = await axios.get(url);
-  //   //console.log(output);
-  //   const {data} = await axios.get(url);
-  //   const imgSource = data.data.images.downsized_large.url;
-  //   console.log(imgSource);
-  //   setGif(imgSource);
-  //   setLoading(false);
-  // }
-
-  // useEffect( ()=> {
-  //   fetchData();
-  // }, [] )
-
+  
   const {gif,loading,fetchData} = useGif(tag);
 
   function handleClick(){
